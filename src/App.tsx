40137ef9de80d6c3ccba6f232f3b64cd33b98d81
Thamarai <tmanalan710@gmail.com>
2025-05-13 11:45:25 +0530

    Navbar Completed but wanna add animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
+import React, { useEffect, useState } from "react"
 import { Nav } from "./components/ui/Nav"
 
 
 
 
-const App = () => {
- 
+const App:React.FC = () => {
+   const [toggle , SetToggle] = useState<boolean>(false)
+
+    useEffect(()=>{
+        toggle ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark')
+    },[toggle])
+
+    const toggleFunction = ():void => {
+        SetToggle(!toggle)
+    }
+   
   return <>
     <div
-      className="absolute inset-0 h-full w-full bg-white bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"
+      className="absolute inset-0 h-full w-full bg-white bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] dark:bg-[#323232]"
     >
-      <Nav />
+      <Nav toggle={toggle} toggleFunction={toggleFunction}/>
     </div>
   </>
 }
--- src/components/ui/Nav.tsx
+++ src/components/ui/Nav.tsx
@@ -1,60 +1,95 @@
+import { useState } from "react"
+import { Burger } from "../../icons/Burger"
 import { Github } from "../../icons/github"
-import { Linkedin } from "../../icons/Linledin"
-import { Leetcode } from "../../icons/Leetcode"
-import { X } from "../../icons/X"
 import { Home } from "../../icons/Home"
+import { Leetcode } from "../../icons/Leetcode"
+import { Linkedin } from "../../icons/Linledin"
+import { Moon } from "../../icons/Moon"
+import { Projects } from "../../icons/Projects"
 import { Resume } from "../../icons/Resume"
 import { User } from "../../icons/User"
-import { Moon } from "../../icons/Moon"
+import { X } from "../../icons/X"
 import { Sun } from "../../icons/Sun"
-import { Projects } from "../../icons/Projects"
-import { useEffect, useState } from "react"
+import { Backspace } from "../../icons/Backspace"
 
-export const Nav = ()=>{
-    const [Toggle , SetToggle] = useState<boolean>(false)
+type NavProps = {
+    toggle:boolean,
+    toggleFunction:()=>void
+}
 
-    useEffect(()=>{
-      if(Toggle){
-        document.documentElement.classList.add('dark')
-      }
-      else{
-        document.documentElement.classList.remove('dark')
-      }
-    },[Toggle])
+export const Nav:React.FC<NavProps> = ({toggle,toggleFunction})=>{
+
   
-    const ToggleFunction = ()=>{
-      SetToggle(!Toggle)
+    const [isOpen , SetisOpen] = useState<boolean>(false)
+
+   
+
+    const burgerOpen = ()=>{
+        SetisOpen(!isOpen)
     }
-  
+    const NavHoverStyles = "hover:bg-[#3f3f44] hover:rounded p-3 hover:text-blue-300"
     return <>
-    
-    <div className="md:w-full sm:w-3xl flex justify-center p-2 text-white dark:text-black ">
-        <div className="bg-[#2b2b2b] w-4xl flex px-2 py-4 rounded-xl justify-center dark:bg-white dark:border-1 dark:border-black">
-            <div className="w-full flex justify-between pr-4 items-center">
-                <div className="">
-                    <span className="bg-white text-black p-3 px-5 rounded-xl dark:border-1 darl:border-black dark:bg-[#2b2b2b] dark:text-white">T.</span>
+           <div className={`${isOpen ? "hidden" : "flex justify-center"}`}>
+             <div className="flex justify-between items-center w-[250px] sm:w-[400px] md:w-[500px] lg:w-[600px] xl:w-[700px] bg-[#323232] py-4 px-2 mt-4 rounded-xl dark:bg-[#cacaca] dark:text-black text-white">
+                <div className="pr-10">
+                    <span className="bg-white text-black py-3 px-4 rounded-xl  dark:bg-black dark:text-white">T.</span>
                 </div>
-                <div className="flex items-center gap-5">
-                    <span><Home/></span>
-                    <span><Projects/></span>
-                    <span><Resume/></span>
-                    <span><User/></span>
-                </div>
-            </div>
-            <div className="border-l border-gray-500"></div>
-            <div className="flex w-full justify-between items-center pl-5">
-                <div className="flex  items-center gap-5">
-                    <span><Github/></span>
-                    <span><Linkedin/></span>
-                    <span><Leetcode/></span>
-                    <span><X/></span>
-                </div>
-                <div className="px-2 items-center">
-                    <button onClick={ToggleFunction} >{Toggle ? <Moon/> : <Sun/>}</button>
+                 {/* Mobile Menu Icon */}
+                <div className="md:hidden px-1" onClick={burgerOpen}>
+                    <Burger />
                 </div>
 
+                {/* Desktop Navigation */}
+                <div className="hidden md:flex gap-5 w-full ml-4 items-center">
+                    <ul className="flex gap-4">
+                        <li><Home/></li>
+                        <li><Resume/></li>
+                        <li><User/></li>
+                        <li><Projects/></li>
+                    </ul>
+                    <hr  className="w-2"/>
+                    <div className="flex justify-between w-full items-center">
+                        <div className="">
+                            <ul className="flex gap-4 items-center">
+                            <li><Github/></li>
+                            <li><Leetcode/></li>
+                            <li><Linkedin/></li>
+                            <li><X/></li>
+                            </ul>
+                        </div>
+                        <div className="px-2" onClick={toggleFunction}>
+                            {toggle ? <Moon/> : <Sun/>}
+                        </div>
+                    </div>
+                </div>
             </div>
+           </div>
+
+        <div className={`${isOpen ? "bg-[#323232] text-white w-full h-full md:hidden" : "hidden" }`}>
+                <nav className={` flex justify-end p-8 `} onClick={burgerOpen}>
+                    <Backspace />
+                </nav>
+                <div className="p-10 text-xl">
+                    <div className="flex justify-center w-full items-center ">
+                        <div className="flex flex-col">
+                        <div className={NavHoverStyles}>Home</div>
+                        <div className={NavHoverStyles}>Resume</div>
+                        <div className={NavHoverStyles}>User</div>
+                        <div className={NavHoverStyles}>Projects</div>
+                        </div>
+                    </div>
+                    <hr className="h-2 m-10"/>
+                    <div className="flex justify-center w-full items-center">
+                        <div className="flex flex-col">
+                        <div className={NavHoverStyles}>Github</div>
+                        <div className={NavHoverStyles}>LinkedIn</div>
+                        <div className={NavHoverStyles}>Leetcode</div>
+                        <div className={NavHoverStyles}>X</div>
+                        </div>
+                    </div>
+                </div>
         </div>
-    </div>
+       
+           
     </>
 }
